03473b0a5a23ffbb492d3351c75050f64a3b4e43
daryakim <lika021097@gmail.com>
2025-03-15 11:27:53 +0900

    Initial commit

---
+++ src/main/java/web/CarService.java
@@ -0,0 +1,25 @@
+package web;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+    private List<Car> cars;
+    public CarService() {
+        cars = new ArrayList<>();
+        cars.add(new Car("Model S", "Tesla", 2022));
+        cars.add(new Car("Mustang", "Ford", 2025));
+        cars.add(new Car("Civic", "Honda", 2013));
+        cars.add(new Car("Accord", "Honda", 2021));
+        cars.add(new Car("Camry", "Toyota", 2020));
+    }
+    public List<Car> getCars(int count) {
+        if (count >= 5) {
+            return cars;
+        }
+        return cars.subList(0, Math.min(count, cars.size()));
+    }
+}
